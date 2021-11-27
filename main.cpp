eda0869448b4d726dfbd7ca44b1df4b2bfe0ba5e
karthikelango137 <94198984+karthikelango137@users.noreply.github.com>
2021-11-27 18:22:19 -0500

    Add files via upload
    colors added in but draw two does not work

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//#include "pch.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,21 +7,24 @@
 #include <iterator>
 #include <stdio.h>
 #include "string.h"
+#include "time.h"
+#include "stdlib.h"
+#include "windows.h"
+#include <cstdio>
 using namespace std;
 
-class Card{
+class Card {
 public:
     int num;
     string color;
-    Card(){
+    Card() {
         num = 10;
-        color = "default";
+        color = "r";
     }
 };
 
-class Deck{
+class Deck {
 public:
-
     vector<Card> cardDeck;
     Deck() {
         for (int i = 0; i < 20; i++) {//19 red cards 1 0 and 2 of 1-9
@@ -47,69 +51,83 @@ public:
             card.color = "y";
             cardDeck.push_back(card);
         }
-        random_shuffle(cardDeck.begin(),cardDeck.end());
+        for(int i = 0; i < 2; i++){
+            Card card;
+            card.num = 2;
+            card.color = "+r";
+            cardDeck.push_back(card);
+        }
+        for(int i = 0; i < 2; i++){
+            Card card;
+            card.num = 2;
+            card.color = "+y";
+            cardDeck.push_back(card);
+        }
+        for(int i = 0; i < 2; i++){
+            Card card;
+            card.num = 2;
+            card.color = "+g";
+            cardDeck.push_back(card);
+        }
+        for(int i = 0; i < 2; i++){
+            Card card;
+            card.num = 2;
+            card.color = "+b";
+            cardDeck.push_back(card);
+        }
+        random_shuffle(cardDeck.begin(), cardDeck.end());
     }
-/*
-    void shuffle(){
-       for(int i = 0; i < rand(); i++){
-           random_shuffle(cardDeck.begin(),cardDeck.end());
-       }
-    }//function to shuffle deck and we take first 7 or top to put in a hand instead of randomizing
-*/
-    void print(){
-        for(int i = 0; i < cardDeck.size(); i++){
+
+    void print() {
+        for (int i = 0; i < cardDeck.size(); i++) {
             cout << cardDeck[i].num << cardDeck[i].color << ' ';
         }
         cout << endl;
-    }//for testing purposes only
+    }
 };
-/*
-class hand{
-    list<Card> handList;
 
-};
-*/
-class Player{
+class Player {
 public:
     int num;
     list<Card> handList;
 public:
-    virtual void displayHand(){}
-    virtual void pickupCard(){}//return card type????
-    virtual void playCard(){}
-    virtual void sortHand(){}//if needed, we can implement later
-    virtual void deal(){};
+    virtual void displayHand() {}
+    virtual void pickupCard() {}//return card type????
+    virtual void playCard() {}
+    virtual void sortHand() {}//if needed, we can implement later
+    virtual void deal() {};
 };
 
-class humPlayer : public Player{
+class humPlayer : public Player {
 public:
     int num;
     list<Card> handList;
-    humPlayer(){
+    humPlayer() {
         Deck dealDeck;
         //list<Card> returnDeck;
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             handList.push_back(dealDeck.cardDeck[i]);
         }
     }
-    void playCard() override{}//place card from user input
+    void playCard() override {}//place card from user input
     void deal() {
         Deck dealDeck;
         //list<Card> returnDeck;
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             handList.push_back(dealDeck.cardDeck[i]);
         }
     }
 
-    void pickupCard(){
-        int randNum = rand()%80;
+    void pickupCard() {
+        int randNum = rand() % 88;
         Deck dealdeck;
         handList.push_back(dealdeck.cardDeck[randNum]);
+        cout << endl;
     }
-    void removeCard(const Card inputCard){
+    void removeCard(const Card inputCard) {
         list<Card>::iterator it;
-        for(it = this->handList.begin(); it != this->handList.end();it++){
-            if(it->num == inputCard.num && it->color == inputCard.color) {
+        for (it = this->handList.begin(); it != this->handList.end(); it++) {
+            if (it->num == inputCard.num && it->color == inputCard.color) {
                 this->handList.erase(it++);
                 break;//to only erase first instance
             }
@@ -118,21 +136,31 @@ public:
         cout << endl;
     }
 
-    bool isLegal(Card playerCard, Card centerCard){
+    bool isLegal(Card playerCard, Card centerCard) {
         //check if card is in hand
         //if card matches centerCard
+
         bool legal = false;
         list<Card>::iterator it;
-        for(it = this->handList.begin(); it != this->handList.end();it++){
-            if(it->num == playerCard.num && it->color == playerCard.color) {
+        for (it = this->handList.begin(); it != this->handList.end(); it++) {
+            if (it->num == playerCard.num && it->color == playerCard.color) {
                 legal = true;
                 break;
             }
             //cout << "iterator: " << it->num << it->color << ' ';
         }
+        if(centerCard.color == "+r")centerCard.color = "r";
+        else if(centerCard.color == "+b")centerCard.color = "b";
+        else if(centerCard.color == "+y")centerCard.color = "y";
+        else if(centerCard.color == "+g")centerCard.color = "g";
 
-        if(legal == true){
-            if(playerCard.num == centerCard.num || playerCard.color == centerCard.color){
+        if(playerCard.color == "+r")playerCard.color = "r";
+        else if(playerCard.color == "+b")playerCard.color = "b";
+        else if(playerCard.color == "+y")playerCard.color = "y";
+        else if(playerCard.color == "+g")playerCard.color = "g";
+
+        if (legal == true) {
+            if (playerCard.num == centerCard.num || playerCard.color == centerCard.color) {
                 legal = true;
             }
             else legal = false;
@@ -140,45 +168,94 @@ public:
 
         return legal;
     }
-    void print(){
+
+    bool anyLegalCard( Card centerCard) {
+        //check if card is in hand
+        //if card matches centerCard
+        bool legal = false;
+        list<Card>::iterator it;
+        for (it = this->handList.begin(); it != this->handList.end(); it++) {
+            if(it->color == "+r")it->color == "r";
+            else if (it->color == "+b")it->color == "b";
+            else if (it->color == "+g")it->color == "g";
+            else if (it->color == "+y")it->color == "y";
+            if (it->num == centerCard.num || it->color == centerCard.color) {
+                legal = true;
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+    void drawTwo(Card inputCard){
+
+    }
+    void print() {
         cout << "Player " << num << "'s Hand: ";
-        for (auto v : handList){
-            std::cout << v.num << v.color << ' ';
+        for (auto v : handList) {
+            if(v.color == "r" || v.color == "+r"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,12);
+                std::cout << v.num << v.color << ' ';
+            }
+            else if (v.color == "b" || v.color == "+b"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,9);
+                std::cout << v.num << v.color << ' ';
+            }
+            else if (v.color == "g"|| v.color == "+g"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,10);
+                std::cout << v.num << v.color << ' ';
+            }
+            else if (v.color == "y"|| v.color == "+y"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,14);
+                std::cout << v.num << v.color << ' ';
+            }
+            else std::cout << v.num << v.color << ' ';
         }
         cout << endl;
     }//for testing purposes only
 };
 
-class cpuPlayer : public Player{
+class cpuPlayer : public Player {
     // no display hand
-    void playCard() override{}//place random card for now
-    void pickupCard() override{}//pickup top card of deck. should be same for humPlayer
+    void playCard() override {}//place random card for now
+    void pickupCard() override {}//pickup top card of deck. should be same for humPlayer
 
 };
 
-Card convertInput(string Card){
+Card convertInput(string Card) {
     //convert user input string to a Card type
     class Card test;
-    test.num = stoi(Card);
-    test.color = Card[1];
+    if(Card[1] != '+'){
+        test.num = stoi(Card);
+        test.color = Card[1];
+    }
+    else{
+        test.num = stoi(Card);
+        test.color = Card.substr(1,2);
+        }
     return test;
 }
 
-int switchPlayer(int numplayers, int pnowNum){//skip and reverse special cards will be implemented here
+int switchPlayer(int numplayers, int pnowNum) {//skip and reverse special cards will be implemented here
     //Player nextPlayer;
     int next;
-    if(numplayers==2){
-        if(pnowNum == 1) next = 2;
+    if (numplayers == 2) {
+        if (pnowNum == 1) next = 2;
         else if (pnowNum == 2) next = 1;
     }
-    else if(numplayers==4){
-        if(pnowNum == 1)next = 2;
+    else if (numplayers == 4) {
+        if (pnowNum == 1)next = 2;
         else if (pnowNum == 2) next = 3;
         else if (pnowNum == 3) next = 4;
         else if (pnowNum == 4) next = 1;
     }
-    else if (numplayers == 3){
-        if(pnowNum == 1)next = 2;
+    else if (numplayers == 3) {
+        if (pnowNum == 1)next = 2;
         else if (pnowNum == 2)next = 3;
         else if (pnowNum == 3) next = 1;
     }
@@ -187,53 +264,172 @@ int switchPlayer(int numplayers, int pnowNum){//skip and reverse special cards w
 }
 
 int main() {
+    srand(time(NULL));
     Deck fullDeck;
     humPlayer p1;
     p1.num = 1;
     humPlayer p2;
     p2.num = 2;
+    humPlayer p3;
+    p3.num = 3;
+    humPlayer p4;
+    p4.num = 4;
     int nextNum = 0;
-    int numPlayers = 2;
+    int numPlayers = 4;
     humPlayer pNow;
     pNow = p1;
+    bool end = false;
+    bool drawTwo;
 
-    while(1){
-        p2.num = 2;
-        p1.num = 1;
-    int randNum = rand()%80;
+    int randNum = rand() % 88;
     Card centerCard = fullDeck.cardDeck[randNum];
-    cout << "Card to Play: " << centerCard.num << centerCard.color << endl ;
-    pNow.print();
-    cout << "Your Turn, Play or Pickup a Card: ";
+    //system("Color 1B");
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole,15);
+    cout <<  "Welcome to UNO!" << endl << "You may type end at any point as your card to end the game." << endl << endl;
     string inputCard;
-    cin >> inputCard;
-    if(inputCard == "pickup"){
-        pNow.pickupCard();
-    }
-    else {
-        Card cardtoRemove = convertInput(inputCard);
-        bool legal = pNow.isLegal(cardtoRemove, centerCard);
-        if (legal) {
-            pNow.removeCard(cardtoRemove);
-        } else {
-            cout << "ILLEGAL MOVE, DRAW OR PLAY ANOTHER CARD" << endl;
-        }
-    }
-    if(pNow.num == 1){
-        p1 = pNow;
-        p1.num = 1;
-    }
-    else if (pNow.num == 2){
-        p2 = pNow;
-        p2.num = 2;
-    }
+    //begin the main while loop
+    while (!end) {
 
-    nextNum = switchPlayer(numPlayers, pNow.num);
-    if(nextNum == 2) pNow = p2;
-    else if (nextNum == 1)pNow = p1;
-        if(pNow.handList.empty()== true){
-            break;
+        //prompts for user
+        bool promptAgain = true;
+        //drawTwo = false;
+        while (promptAgain) {
+            cout << "Card to Play: ";
+            if(centerCard.color == "r" || centerCard.color == "+r"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,12);
+                std::cout << centerCard.num << centerCard.color << ' ';
+            }
+            else if (centerCard.color == "b"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,9);
+                std::cout << centerCard.num << centerCard.color << ' ';
+            }
+            else if (centerCard.color == "g"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,10);
+                std::cout << centerCard.num << centerCard.color << ' ';
+            }
+            else if (centerCard.color == "y"){
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole,14);
+                std::cout << centerCard.num << centerCard.color << ' ';
+            }
+            cout << endl;
+            SetConsoleTextAttribute(hConsole,15);
+            pNow.print();
+            SetConsoleTextAttribute(hConsole,15);
+            if (pNow.anyLegalCard(centerCard))
+                cout << "Your Turn, Play or Pickup a Card: ";
+            else
+                cout << "You have no playable cards. You can pickup a card by typing 'pickup'" << endl;
+            //input the card if you can play any card
+            cin >> inputCard;
+            //begin main nested if statement
+            cout <<"322 OUTPUTCARD: "<<inputCard[1]<<endl;
+            if (inputCard == "pickup") {				//pickup case
+                pNow.pickupCard();
+                promptAgain = false;
+            }
+            else if (inputCard[1] == '+'){
+                Card temp = convertInput(inputCard);
+                bool legal = pNow.isLegal(temp, centerCard);
+                if(legal){
+                    drawTwo = true;
+                    Card cardtoRemove = convertInput(inputCard);
+                    pNow.removeCard(cardtoRemove);//remove the card
+                    centerCard = cardtoRemove;//update the ceneter card
+                    //we don't need to prompt this user for more info
+                    promptAgain = false;
+                }
+                else{
+                    cout << "ILLEGAL MOVE, DRAW OR PLAY ANOTHER CARD-------" << endl;
+                    promptAgain = true;
+                }
+            }
+            else if (inputCard == "end") {				//end case
+                cout << "The game has been quit." << endl;
+                end = true;
+                break;
+
+                //else assume that it's a card
+            }else {										//card case
+                Card cardtoRemove = convertInput(inputCard);
+                bool legal = pNow.isLegal(cardtoRemove, centerCard);
+                if (legal) {//if the card is a legal card to be played
+                    pNow.removeCard(cardtoRemove);//remove the card
+                    centerCard = cardtoRemove;//update the ceneter card
+                    //we don't need to prompt this user for more info
+                    promptAgain = false;
+                }
+                else {//catch-all case, illagal move or unexpected input
+                    cout << "ILLEGAL MOVE, DRAW OR PLAY ANOTHER CARD" << endl;
+                    promptAgain = true;
+                }
+            }
         }
+        //win case
+        //will need to add option to play again and restart the game
+        if (pNow.handList.empty() == true) {
+            cout << "Player " << pNow.num << "has won!" << endl;
+            //end game
+            return 0;
+        }
+
+        if (pNow.num == 1) {
+            p1 = pNow;
+            p1.num = 1;
+        }
+        else if (pNow.num == 2) {
+            p2 = pNow;
+            p2.num = 2;
+        }
+        else if (pNow.num == 3) {
+            p3 = pNow;
+            p3.num = 3;
+        }
+        else if (pNow.num == 4) {
+            p4 = pNow;
+            p4.num = 4;
+        }
+
+        //switch players for while loop
+        if(drawTwo){
+            nextNum = switchPlayer(numPlayers, pNow.num);
+            if (nextNum == 1)
+                pNow = p1;
+            else if (nextNum == 2)
+                pNow = p2;
+            else if (nextNum == 3)
+                pNow = p3;
+            else if (nextNum == 4)
+                pNow = p4;
+            pNow.pickupCard();
+            pNow.pickupCard();
+            nextNum = switchPlayer(numPlayers, pNow.num);
+            if (nextNum == 1)
+                pNow = p1;
+            else if (nextNum == 2)
+                pNow = p2;
+            else if (nextNum == 3)
+                pNow = p3;
+            else if (nextNum == 4)
+                pNow = p4;
+            drawTwo = false;
+        }
+        else{
+            nextNum = switchPlayer(numPlayers, pNow.num);
+            if (nextNum == 1)
+                pNow = p1;
+            else if (nextNum == 2)
+                pNow = p2;
+            else if (nextNum == 3)
+                pNow = p3;
+            else if (nextNum == 4)
+                pNow = p4;
+        }
+
     }
 
     return 0;
